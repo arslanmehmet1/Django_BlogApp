9e410b75f8e8efdff13a10dcfd68a5411142f466
Mehmet Arslan <marslan.developer@gmail.com>
2023-01-02 22:05:44 +0300

    sencronize

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-
 from rest_framework.viewsets import ModelViewSet
+
 from .models import Category, Blog
-from .serializers import CategorySerializer
-class CategoryView(ModelViewSet): #crud
+from .serializers import CategorySerializer, BlogSerializer
+
+
+class CategoryView(ModelViewSet):  # crud
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-
+class BlogView(ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
